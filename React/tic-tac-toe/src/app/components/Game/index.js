7b605c03d7ae0c3022f3a6f500374d9f662b8b08
davidtito1992 <tdavid2000@hotmail.com>
2018-10-01 17:20:27 -0300

    starter code

---
+++ React/tic-tac-toe/src/app/components/Game/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import game from './game.scss';
+import Board from '../Board';
+
+class Game extends React.Component {
+    render() {
+      return (
+        <div className={game.game}>
+          <div className={game.gameBoard}>
+            <Board />
+          </div>
+          <div className={game.gameInfo}>
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  export default Game ;
---
+++ React/tic-tac-toe/src/app/components/Square/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import square from './square.scss';
+
+class Square extends Component {
+    render() {
+      return (
+        <button className={square.square}>
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  export default Square;
+  
