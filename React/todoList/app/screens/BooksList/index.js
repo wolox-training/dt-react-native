f15d2d08929cbbb69209a5e184310e7d1f256c34
davidtito1992 <tdavid2000@hotmail.com>
2018-10-22 15:46:37 -0300

    Componenetes-II-listado-de-libros card

--- React/todoList/app/screens/BooksList/index.js
+++ React/todoList/app/screens/BooksList/index.js
@@ -1,15 +1,55 @@
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { Text, Image, ListView, View } from "react-native";
+import Books from "../../../books";
+import { ListItem } from "react-native-elements";
 
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 != r2 });
+const defaultImage =
+  "https://acompli.helpshift.com/static/images/avatars/default-app-icon.png";
 
 class BookScreen extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookList: ds.cloneWithRows([]),
+      loaded: false
+    };
+  }
+
+  render() {
+    if (this.state.loaded) {
       return (
-        <View>
-        <Text> Otro Navigation</Text>
-      </View>
+        <ListView
+          initialListSize={5}
+          enableEmptySections={true}
+          dataSource={this.state.bookList}
+          renderRow={book => {
+            return this.renderbookRow(book);
+          }}
+        />
       );
+    } else {
+      return <Text>Loading Books...</Text>;
     }
   }
+
+  renderbookRow(book) {
+    return ( <ListItem
+              roundAvatar
+              title={`${book.title}`}
+              subtitle={book.author}
+              avatar={{ uri: `${book.image_url ? book.image_url : defaultImage}` }}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />);
+  }
+  componentDidMount() {
+    Promise.resolve(JSON.parse(JSON.stringify(Books))).then(data => {
+      this.setState({
+        bookList: ds.cloneWithRows(data),
+        loaded: true
+      });
+    });
+  }
+}
 
 export default BookScreen;
